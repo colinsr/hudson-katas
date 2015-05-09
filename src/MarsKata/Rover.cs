f5be75d3b783016c30005f6ff69843ee97c6023d
Eric Fleming <eric-fleming18@hotmail.com>
2015-05-08 21:16:06 -0400

    Red - MoveRoverForwardOneUnit

---
+++ src/MarsKata/Rover.cs
@@ -0,0 +1,8 @@
+﻿namespace MarsKata
+{
+    public class Rover
+    {
+        public int XAxis { get; set; }
+        public int YAxis { get; set; }
+    }
+}
---
+++ src/UnitTests/MoveRoverTests/MoveShould.cs
@@ -0,0 +1,24 @@
+﻿using MarsKata;
+using MarsKata.MoveRover;
+using NUnit.Framework;
+
+namespace UnitTests.MoveRoverTests
+{
+    [TestFixture]
+    public class MoveShould
+    {
+        [Test]
+        public void MoveRoverForwardOneUnit()
+        {
+            var rover = new Rover();
+            
+            //Refactor this (A new rover should probably be initialized at (0,0)
+            rover.XAxis = 0;
+            rover.YAxis = 0;
+
+            var roverMover = new RoverMover();
+            roverMover.Move();
+            Assert.AreEqual(1, rover.XAxis);
+        }
+    }
+}
