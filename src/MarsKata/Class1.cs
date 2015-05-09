7b5ff3d099d0b7452d441a033fdf1dd2f6215744
Eric Fleming <eric-fleming18@hotmail.com>
2015-05-08 20:48:11 -0400

    Created core project and Unit Tests project

---
+++ src/MarsKata/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsKata
+{
+    public class Class1
+    {
+    }
+}
